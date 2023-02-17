ec75beefd6c7673a6406e42c46b3d9501774952e
Masha <mariia.tabala@gmail.com>
2023-02-16 19:22:51 -0800

    Comment out content of ul shows list in HTML shows page and create an array for the shows info in js shows page.

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,62 @@
+// create an array for the shows
+const showsInfo = [
+    {
+    subheader1: "date",
+    subheader2: "venue",
+    subheader3: "location",
+    date: "Mon Sept 06 2021",
+    venue: "Ronald Lane",
+    location: "San Francisco, CA",
+    button: "Buy Tickets"
+    },
+    
+    {
+    subheader1: "date",
+    subheader2: "venue",
+    subheader3: "location",
+    date: "Tue Sept 21 2021",
+    venue: "Pier 3 East",
+    location: "San Francisco, CA",
+    button: "Buy Tickets"
+    },
+    
+    {
+    subheader1: "date",
+    subheader2: "venue",
+    subheader3: "location",
+    date: "Fri Oct 15 2021",
+    venue: "View Lounge",
+    location: "San Francisco, CA",
+    button: "Buy Tickets"
+    },
+    
+    {
+    subheader1: "date",
+    subheader2: "venue",
+    subheader3: "location",
+    date: "Sat Nov 06 2021",
+    venue: "Hyatt Agency",
+    location: "San Francisco, CA",
+    button: "Buy Tickets"
+    },
+    
+    {
+    subheader1: "date",
+    subheader2: "venue",
+    subheader3: "location",
+    date: "Fri Nov 26 2021",
+    venue: "Moscow Center",
+    location: "San Francisco, CA",
+    button: "Buy Tickets"
+    },
+    
+    {
+    subheader1: "date",
+    subheader2: "venue",
+    subheader3: "location",
+    date: "Wed Dec 15 2021",
+    venue: "Press Club",
+    location: "San Francisco, CA",
+    button: "Buy Tickets"
+    },
+]
