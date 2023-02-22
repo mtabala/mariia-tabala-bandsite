c01c59216db72b088e84d1bc673a816db871f364
Masha <mariia.tabala@gmail.com>
2023-02-21 16:15:00 -0800

    Comment out an array to keep it empty

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -5,25 +5,7 @@ const apiURL = "https://project-1-api.herokuapp.com";
 const apiKey = "6621de2c-5d7f-49e0-ad3e-6ba13f94538c";
 const target = "comments"
 // create an array with objects to push to and render to DOM
-const commentsArr = [
-    {
-    name: "Connor Walton",
-    date: "02/17/2021",    
-    comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-    },
-
-    {
-    name: "Emilie Beach",
-    date: "01/09/2021",    
-    comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-    },
-
-    {
-    name: "Miles Acosta",
-    date: "12/20/2020",    
-    comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-    },    
-];
+const commentsArr = [];
 //add even listener to submit the new comment and push it to the array
 commentForm.addEventListener("submit", (event) => {
     event.preventDefault();
